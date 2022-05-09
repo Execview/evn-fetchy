25866a1d18ea758b06c171cf586fcc58ac8dfc17
Namyts <35004248+Namyts@users.noreply.github.com>
2022-05-09 12:56:53 +0100

    removed req_id. abort controller is only added if there is a timeout. timeout defualt is removed. content-type is deleted if undefined

--- src/fetchy.js
+++ src/fetchy.js
@@ -3,7 +3,7 @@ import AC from 'abort-controller';
 import crossBtoa from 'btoa-lite'
 
 const removeOurOptions = (options) => {
-	const {body,debug,token,basic,method,timeout,headers,preview,jest,notJSON,leaveError,req_id, ...otherOptions} = options
+	const {body,debug,token,basic,method,timeout,headers,preview,jest,notJSON,leaveError, ...otherOptions} = options
 	return otherOptions
 }
 
@@ -19,8 +19,7 @@ export const fetchy = (url,options={}) => {
 	const leaveError = options.leaveError || previewMode
 	const token = options.token
 	const basic = options.basic
-	const req_id = options.req_id
-	const timeout = options.timeout || 60000
+	const timeout = options.timeout
 	const otherOptions = removeOurOptions(options)
 
 
@@ -29,28 +28,34 @@ export const fetchy = (url,options={}) => {
 
 	let headers = {}
 	if(method!=='GET'){headers["Content-Type"] = "application/json"}
-	if(req_id){headers['x-request-id'] = req_id}
+	if(token){headers["Authorization"] = `Bearer ${token}`}
+	if(basic){
+		let basicString = ''
+		if(typeof(basic)==='string'){
+			basicString = basic
+		} else {
+			if(basic.user && basic.password){
+				basicString = toBase64(`${basic.user}:${basic.password}`)
+			} else {
+				return Promise.reject('provide user and password for basic auth!')
+			}
+		}
+		headers["Authorization"] = `Basic ${basicString}`
+	}
 	
-	let controller = new AbortControllerClass();
+	let controller = null; //for controller.signal if a timeout is set
 	let fetchOptions = {
-		signal: controller.signal,
 		method:method,
 		headers: {...headers, ...options.headers},
 		...otherOptions
 	}
+	if(!fetchOptions.headers["Content-Type"]){ delete fetchOptions.headers["Content-Type"] }
 	if(hasBody){
-		const contentType = fetchOptions.headers["Content-Type"] || ''
-		let fetchBody = body
-		if(typeof(body)!=="string" && ['json','text'].some(t=>contentType.includes(t))){fetchBody = JSON.stringify(fetchBody)}
-		fetchOptions.body = fetchBody
+		fetchOptions.body = (typeof(body)!=="string" && (fetchOptions.headers["Content-Type"]||'').includes("json")) ? JSON.stringify(body) : body
 	}
-	
-	if(token){fetchOptions.headers["Authorization"] = `Bearer ${token}`}
-	if(basic){
-		const alreadyString = typeof(basic)==='string'
-		if(!alreadyString && (!basic.user || !basic.password)){return Promise.reject('provide user and password for basic auth!')}
-		const basicString = alreadyString ? basic : toBase64(`${basic.user}:${basic.password}`)
-		fetchOptions.headers["Authorization"] = `Basic ${basicString}`
+	if(timeout){
+		controller = new AbortControllerClass()
+		fetchOptions.signal = controller
 	}
 
 	const debugLog = {url: url, fetchOptions: {...fetchOptions, body:body}}
@@ -63,10 +68,11 @@ export const fetchy = (url,options={}) => {
 	}
 	
 	if(debug){fetchPromise = fetchPromise.then((res)=>{console.log(res); return res})}
-	return Promise.race([
+	
+	return !timeout ? fetchPromise : Promise.race([
 		fetchPromise,
 		new Promise((resolve, reject) => setTimeout(() => {reject('too slow! -> '+url); controller.abort()}, timeout))
-	])
+	]) 
 }
 
 export default fetchy
